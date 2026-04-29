c718aa9292eb6a48606e274f45e5f84a2452630e
athmanar <athma.lakshmi.narayanan@intel.com>
2026-04-29 15:39:33 -0700

    Update .gitignore and add dataset configuration files for new datasets

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from dassl.utils import setup_logger, set_random_seed, collect_env_info
 from dassl.config import get_cfg_default
 from dassl.engine import build_trainer
 
-# custom
+# # custom
 import datasets.oxford_pets
 import datasets.oxford_flowers
 import datasets.fgvc_aircraft
